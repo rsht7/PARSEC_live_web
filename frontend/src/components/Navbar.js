8898c8785f74aa695eb4b8a4418433dcaef54827
Siddharth Khati <siddharth.khati03@gmail.com>
2024-06-03 09:28:56 +0530

    Modify Navbar.js with new changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,109 +1,45 @@
-import {Link} from 'react-router-dom'
-import fulllogo from './compassets/fulllogo.png'
-// import twitter from './twitter.png'
-// import facebook1 from './facebook1.png'
-// import youtube1 from './youtube1.png'
-// import whatsapp1 from './whatsapp1.png'
-
-import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import fulllogo from './compassets/fulllogo.png';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [showNav, setShowNav] = useState(false);
 
-    // const [isScrolling, setIsScrolling] = useState(false);
-
-    // useEffect(() => {
-    // let timeoutId;
-
-    // const handleScroll = () => {
-    //   setIsScrolling(true);
-    //   clearTimeout(timeoutId);
-    //   timeoutId = setTimeout(() => setIsScrolling(false), 3000);
-    // };
-
-    // window.addEventListener('scroll', handleScroll);
-
-    // return () => {
-    //   window.removeEventListener('scroll', handleScroll);
-    // };
-    // }, []);
-    const [isScrollingUp, setIsScrollingUp] = useState(true);
-    const [lastScrollY, setLastScrollY] = useState(0);
-
-    const handleScroll = () => {
-        if (window.scrollY < lastScrollY) {
-        setIsScrollingUp(true);
-        } else {
-        setIsScrollingUp(false);
-        }
-     setLastScrollY(window.scrollY);
+    const toggleNav = () => {
+        setShowNav(!showNav);
     };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [lastScrollY]);
-
-
-
-
-
-    return(
-        // <header className='header'>
-
-         <header  className={`header ${isScrollingUp ? 'show' : 'hide'}`}> 
-            
-            {/* <div className='prenav'>
-                <p>Phone : +1 (123) 456-7890 - Email : contact@parseclive.com</p>
-                <ul>
-                    <li><img src={facebook1}></img></li>   
-                    <li><img src={twitter}></img></li>
-
-                    <li><img src={youtube1}></img></li>
-                    <li><img src={whatsapp1}></img></li>
-                </ul>
-            </div> */}
-            <nav className='navbar'>
-                
+    return (
+        <header className={`header ${showNav ? 'show-nav' : ''}`}>
+            <nav className="navbar">
                 <h1>
-                    <Link to='/' >
-                    <img src={fulllogo}></img>
-                    </Link> 
+                    <Link to="/">
+                        <img src={fulllogo} alt="Logo" />
+                    </Link>
                 </h1>
-                <ul className='nav-links'> 
-                    <li><Link to='/'>
-                            HOME
-                        </Link>
-                    </li>    
-                           
-                    <li><Link to='/event'>
-                            EVENTS
-                        </Link>
-                    </li> 
+                <div className={`hamburger ${showNav ? 'open' : ''}`} onClick={toggleNav}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <ul className={`nav-links ${showNav ? 'show' : ''}`}>
                     <li>
-                        <Link to='/about'>
-                             ABOUT US
-                        </Link>
-                    </li> 
+                        <Link to="/">HOME</Link>
+                    </li>
                     <li>
-                        <Link to='/contactus'>
-                             CONTACT
-                         </Link>
-                    </li> 
-
+                        <Link to="/event">EVENTS</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">ABOUT US</Link>
+                    </li>
+                    <li>
+                        <Link to="/contactus">CONTACT</Link>
+                    </li>
                 </ul>
-                {/* <Link className='cart-btn'  to=''>CART</Link> */}
-                <button className='cart-btn'>CART</button>
-                
-                      
-            </nav> 
-                
-                                         
+                <button className="cart-btn">CART</button>
+            </nav>
         </header>
-            
-    
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
