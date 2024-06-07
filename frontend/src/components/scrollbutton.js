8634f9205d4c2a7394f276e6ac97f2eb720abc63
Siddharth Khati <siddharth.khati03@gmail.com>
2024-06-07 13:09:18 +0530

    Add scroll button functionality to hide when at top of section and show on scroll

--- frontend/src/components/scrollbutton.js
+++ frontend/src/components/scrollbutton.js
@@ -1,27 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import topbtn4 from './compassets/topbtn4.png';
 
-import topbtn4 from './compassets/topbtn4.png'
+const Scrollbtn = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
+  const handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    setIsVisible(scrollTop > 0);
+  };
 
-const Scrollbtn = ()=> {
-    const scrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth" // Smooth scrolling behavior
-      });
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
-  
-    return (
-      <div className="scrollbtn" >  
-        
-        <img
-            src={topbtn4}
-            alt="Top"
-            className="scroll-top-icon"
-            onClick={scrollToTop}
-        />
+  }, []); 
 
-      </div>
-    );
-  }
-  
-  export default Scrollbtn;
+  return (
+    <div className="scrollbtn" style={{ display: isVisible ? 'block' : 'none' }}>
+      <img
+        src={topbtn4}
+        alt="Top"
+        className="scroll-top-icon"
+        onClick={scrollToTop}
+      />
+    </div>
+  );
+};
+
+export default Scrollbtn;
